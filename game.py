45c5190aaaed34411f18df0df3509e2cd493303d
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-04 07:57:40 -0500

    Coding For basics

---
+++ game.py
@@ -0,0 +1,12 @@
+import turtle as t
+
+window = t.Screen()
+window.title("Trial Game")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+
+#Main game loop
+while True:
+    window.update()
