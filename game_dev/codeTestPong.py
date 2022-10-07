6a78356bb23633dac1f6be3970868c81d923d875
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-06 22:20:24 -0500

    Added code for moving Paddle B and win con

--- game_dev/codeTestPong.py
+++ game_dev/codeTestPong.py
@@ -31,6 +31,8 @@ ball.shape("circle") ## Different Ball Shape
 ball.color("blue")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2 ## Testing Change (Might edit, Default = 2)
 
 #Function 
 def padA_up():
@@ -38,18 +40,52 @@ def padA_up():
     if y >= 250: ## The paddle has center of 40 pixels and window size is 300 pixels either direction
         return
     y += 20 ## Add 20 pixels to y
-    padA.sety(y) ## Sets the y coordinate to 20
+    padA.sety(y) ## Sets the y coordinate to 20 above
 
 def padA_down():
     y = padA.ycor() ## Getting the y coordinate of turtle object
     if y <= -250: ## The paddle has center of 40 pixels and window size is 300 pixels either direction
         return
-    y -= 20 ## Add 20 pixels to y
-    padA.sety(y) ## Sets the y coordinate to 20
+    y -= 20 ## Removes 20 pixels to y
+    padA.sety(y) ## Sets the y coordinate to 20 below
+
+def padB_up():
+    y = padB.ycor() ## Getting the y coordinate of turtle object
+    if y >= 250: ## The paddle has center of 40 pixels and window size is 300 pixels either direction
+        return
+    y += 20 ## Add 20 pixels to y
+    padB.sety(y) ## Sets the y coordinate to 20 below
+
+def padB_down():
+    y = padB.ycor() ## Getting the y coordinate of turtle object
+    if y <= -250: ## The paddle has center of 40 pixels and window size is 300 pixels either direction
+        return
+    y -= 20 ## Removes 20 pixels to y
+    padB.sety(y) ## Sets the y coordinate to 20 below
+
+def ball_move():
+    if ball.ycor() > 285: ## Boundary Check
+        ball.sety(285) ## Ball Correction ## Note: Don't make the mistake of ".ycor =" and "sety()"
+        ball.dy *= -1 ## Reverse Ball Speed
+    if ball.ycor() < -285: ## Bottom Correction
+        ball.sety(-285)
+        ball.dy *= -1 
+    ball.setx(ball.xcor() + 0.05*ball.dx)
+    ball.sety(ball.ycor() + 0.05*ball.dy)
+
+def win_check():
+    if ball.xcor() > 385:
+        ball.goto(0, 0)
+        ball.dx = -1
+    if ball.xcor() < -385:
+        ball.goto(0, 0)
+        ball.dx = 1
 
 window.listen()
 window.onkeypress(padA_up, "w")
 window.onkeypress(padA_down, "s")
+window.onkeypress(padB_up, "Up")
+window.onkeypress(padB_down, "Down")
 
 ##def Shoot():
   ##  bullet = t.Turtle()
@@ -70,3 +106,5 @@ window.listen()
 #Main game loop
 while True:
     window.update()
+    ball_move()
+    win_check()
