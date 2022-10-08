8ff7f724e51982954c54a92ccf30d7d969b65824
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-08 04:47:08 -0500

    Simple Math Function

--- frontend_dev/sample.js
+++ frontend_dev/sample.js
@@ -1,15 +1,14 @@
 function doMath(x, y)
 {
-    const choice = Math.floor(Math.random()*4);
+    choice = Math.floor(Math.random()*4);
     if(choice == 1)
-    return x*y;
+    {return x*y;}
     else if(choice == 2)
-    return x+y;
+    {return x+y;}
     else if(choice == 3)
-    return Math.abs(x-y);
+    {return Math.abs(x-y);}
     else
-    return x%y;
+    {return x%y;}
 }
-
-const result = doMath(Math.floor(Math.random()*10),Math.floor(Math.random()*20));
+result = doMath(Math.floor(Math.random()*10), Math.floor(Math.random()*20));
 console.log(result);
