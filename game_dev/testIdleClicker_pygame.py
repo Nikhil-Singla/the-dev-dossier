5bea89a375faf4d9ea8ec2d72f67cd11524b6a20
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-09 20:42:24 -0500

    Color imports, Initial game settings created

--- game_dev/testIdleClicker_pygame.py
+++ game_dev/testIdleClicker_pygame.py
@@ -4,8 +4,24 @@
 import pygame  
 pygame.init()
 
+## Color Palette = (R, G, B) values
+aqua = (0,255,255)
+navy = (0,0,205)
+black = (0,0,0)
+skin = (255,228,196)
+orange = (255,97,3)
+metallic = (198,226,255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
+## End Palette
+
 screen = pygame.dislpay.set_mode([1080, 720])
 pygame.display.set_caption('Idle Turret Maker')
+
+background = black
+frameRate = 60
+font = pygame.font.Font('comic.ttf', '16')
 
 gameState = True ## Game Running
 while gameState:
