8c9ec35b23304e53a03422b25a801eaf1dafb8c4
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-09 20:28:22 -0500

    Start creation. Exit command. Display setup

--- game_dev/testIdleClicker_pygame.py
+++ game_dev/testIdleClicker_pygame.py
@@ -1,4 +1,17 @@
 ## To use Python in CMD, use py instead of python
-import pygame as game 
-game.init()
+## Idle clicker game, Turret Auto Tower Defense
 
+import pygame  
+pygame.init()
+
+screen = pygame.dislpay.set_mode([1080, 720])
+pygame.display.set_caption('Idle Turret Maker')
+
+gameState = True ## Game Running
+while gameState:
+
+    for event in pygame.event.get():
+        if event.type() == pygame.QUIT: ## Different from quit(). Here, its an event
+            gameState = False
+
+pygame.quit() ## Uninitialize all pygame modules
