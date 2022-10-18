a5af93aee177aa76f9968bb753a5a525fd62c328
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-18 18:54:02 -0500

    Initial particle and movement value definition

--- game_dev/lifeSystemProgramming.py
+++ game_dev/lifeSystemProgramming.py
@@ -1,6 +1,6 @@
 ## Simulate life processes.
 
-import pygame  
+import pygame, random
 pygame.init()
 
 ## Color Palette = (R, G, B) values
@@ -15,10 +15,17 @@ green = (0, 255, 0)
 white = (255, 255, 255)
 ## End Palette
 
-screen = pygame.display.set_mode([1080, 720]) ## Resolution
+screen = pygame.display.set_mode([500, 500]) ## Resolution
 pygame.display.set_caption('Not Adventure Capitalist') ## Title of the game
 background = black  ## Variable that can be edited later
 frameRate = 60  ## Framerate 
 font = pygame.font.Font('freesansbold.ttf', 16) ## Font
 timer = pygame.time.Clock() ## Help run our game at 60 FPS
 
+class Particle():
+    def __init__(self, color, startx=500, starty=500):
+        self.x = random.randint(0, starty)
+        self.y = random.randint(0, starty)
+        self.col = color
+        self.dx = 2
+        self.dy = 2
