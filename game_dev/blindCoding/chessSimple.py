4e7ef5405adeee2c82a55b43d7ef0510730b53a1
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-30 09:31:02 -0500

    New Start

--- game_dev/blindCoding/chessSimple.py
+++ game_dev/blindCoding/chessSimple.py
@@ -15,6 +15,17 @@ white = (255, 255, 255)
 brown = (225, 193, 110)
 ## End Palette
 
+class funcTimer():
+    def __init__(self):
+        self.start = pygame.time.get_ticks()    ## Clock starting
+
+    def run(self, time):
+        self.cooldown = time                    ## Set cooldown = Time (miliseconds)
+        now = pygame.time.get_ticks()           ## Get current time
+        if now - self.start >= self.cooldown:   ## If current time passed is greater than cooldown
+            self.start = now                    ## Change current time
+            return True                         ## Toggle Value of cooldown return 
+
 ## Global Variables
 length_of_screen = 800 
 breadth_of_screen = 800
@@ -30,6 +41,7 @@ clock_start = pygame.time.get_ticks()
 
 ## Game Variables
 
+count = 0
 empty_rows = [0]*8
 board_configuration = []
 board_values = [[i+j for i in range(8)] for j in range(8)]
@@ -40,6 +52,29 @@ the_two_sides = [black, white]
 
 num_piece_per_side = [8, 2, 2, 2, 1, 1] ## For Future Customization
 ## End Game Variables
+
+class ChessPiece():
+    def __init__(self, side, name):
+        self.name = name
+        self.side = side
+        self.value = self.piece_value()
+
+    def piece_value(self):
+        for i in range(len(list_pieceName)):
+            if self.name == list_pieceName[i].lower():
+                return list_pieceValue[i]
+        return 0
+
+## USEFUL FUNCTIONS
+def printList1(input_list):
+    for i in range(len(input_list)):
+        print(input_list[i], sep = ', ')
+
+def printList2(input_list):
+    for i in range(len(input_list)):
+        for j in range(len(input_list[i])):
+            print(input_list[i][j], sep = ',')
+## END OF USEFUL FUNCTIONS
 
 def start_configuration(boolean_random):
     if boolean_random:
@@ -56,3 +91,17 @@ def start_configuration(boolean_random):
 
 start_configuration(False)
 
+gameState = True ## Game Running
+display_screen.fill(background_color) ## Have our initial background on the screen
+while gameState:
+    framerate_timer.tick(frameRate) ## Tick at the specified framerate
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: ## Different from quit(). Here, its an event
+            gameState = False
+
+    pygame.display.flip() ## Update the content of the entire display
+
+printList2(board_configuration)
+printList2(board_values)
+
+pygame.quit() ## Uninitialize all pygame modules
