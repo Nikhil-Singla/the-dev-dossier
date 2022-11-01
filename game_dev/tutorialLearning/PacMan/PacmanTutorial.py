222191cb63d65d534a42587bf5ef802b87f73f38
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-01 07:30:31 -0500

    Importing boards and displaying dots

--- game_dev/tutorialLearning/PacMan/PacmanTutorial.py
+++ game_dev/tutorialLearning/PacMan/PacmanTutorial.py
@@ -1,4 +1,5 @@
 import pygame
+from board import boards
 pygame.init()
 
 ## Color Palette = (R, G, B) values
@@ -13,6 +14,7 @@ green = (0, 255, 0)
 white = (255, 255, 255)
 ## End Palette
 
+level = boards #[active_board]
 width = 900
 height = 950
 
@@ -21,11 +23,21 @@ timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
 
+def draw_board(lvl):
+    tile_height = ((height-50) // 32)   ## 50 Pixels for space to write. And Floor division to round down the tile to the nearest height. 32 Tiles vertically
+    tile_width = ((width)//30)      
+    for i in range(len(lvl)):           ## i iterates across the rows of cells / y axis
+        for j in range(len(lvl[i])):    ## j iterates across the individual cells in the rows / columns / x axis
+            if lvl[i][j] == 1:
+                pygame.draw.circle( screen, white, ( (j*tile_width + (0.5*tile_width)), (i*tile_height + (0.5*tile_height)) ), 3 )
+            if lvl[i][j] == 2:
+                pygame.draw.circle( screen, white, ( (j*tile_width + (0.5*tile_width)), (i*tile_height + (0.5*tile_height)) ), 6 )
+
 gameState = True
 while gameState:
     timer.tick(fps)
     screen.fill(black)
-
+    draw_board(level)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
