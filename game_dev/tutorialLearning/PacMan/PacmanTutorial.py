54e3253cc9243e799a38ab8c0eab5e5252f356b5
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-01 08:07:30 -0500

    Drawing horizontal and vertical lines.

--- game_dev/tutorialLearning/PacMan/PacmanTutorial.py
+++ game_dev/tutorialLearning/PacMan/PacmanTutorial.py
@@ -12,11 +12,14 @@ metallic = (198,226,255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 white = (255, 255, 255)
+blue = (0,0,255)
 ## End Palette
 
-level = boards #[active_board]
+level = boards #[active_board] for multiple levels
 width = 900
 height = 950
+PI = 3.141592653589793
+
 
 screen = pygame.display.set_mode([width, height])
 timer = pygame.time.Clock()
@@ -24,14 +27,21 @@ fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
 
 def draw_board(lvl):
-    tile_height = ((height-50) // 32)   ## 50 Pixels for space to write. And Floor division to round down the tile to the nearest height. 32 Tiles vertically
-    tile_width = ((width)//30)      
+    tile_height = ((height - 50) // 32)   ## 50 Pixels for space to write. And Floor division to round down the tile to the nearest height. 32 Tiles vertically
+    tile_width = ((width) // 30)      
     for i in range(len(lvl)):           ## i iterates across the rows of cells / y axis
         for j in range(len(lvl[i])):    ## j iterates across the individual cells in the rows / columns / x axis
             if lvl[i][j] == 1:
-                pygame.draw.circle( screen, white, ( (j*tile_width + (0.5*tile_width)), (i*tile_height + (0.5*tile_height)) ), 3 )
+                pygame.draw.circle( screen, white, ( (j*tile_width + (0.5*tile_width)), (i*tile_height + (0.5*tile_height)) ), 4 )
             if lvl[i][j] == 2:
-                pygame.draw.circle( screen, white, ( (j*tile_width + (0.5*tile_width)), (i*tile_height + (0.5*tile_height)) ), 6 )
+                pygame.draw.circle( screen, white, ( (j*tile_width + (0.5*tile_width)), (i*tile_height + (0.5*tile_height)) ), 8 )
+            if lvl[i][j] == 3:
+                pygame.draw.line( screen, blue, ( (j*tile_width + (0.5*tile_width)), (i*tile_height) ), 
+                                                ( (j*tile_width + (0.5*tile_width)), (i*tile_height + tile_height) ), 3)
+            if lvl[i][j] == 4:
+                pygame.draw.line( screen, red, ( (j*tile_width), (i*tile_height + (0.5*tile_height)) ), 
+                                                ( (j*tile_width + tile_width), (i*tile_height + (0.5*tile_height)) ), 3)
+
 
 gameState = True
 while gameState:
