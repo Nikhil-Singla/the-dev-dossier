cf815281d7e04aa571e4d4a296e7f98b97bf68cb
Nikhil-Singla <nsingla3.14@gmail.com>
2023-05-10 00:38:21 -0500

    Correct Connection

--- classroom_projectdevelpoment/EECS447/HTML/PHP/Connect.php
+++ classroom_projectdevelpoment/EECS447/HTML/PHP/Connect.php
@@ -5,10 +5,12 @@ $database = "n210s707";
 $name = "n210s707";
 $pass = "nikhil12345";
 
+/*
 $server = "localhost";
 $database = "n210s707";
 $name = "root";
 $pass = "";
+*/
 
 $mysqli = new mysqli($server, $name, $pass, $database);
                      
