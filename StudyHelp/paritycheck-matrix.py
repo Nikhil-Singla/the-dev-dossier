7e4ed6123e93ba1e54badb0e6122bd4fc2c0e22a
Nikhil-Singla <nsingla3.14@gmail.com>
2024-02-23 15:11:27 -0600

    Small Parity

---
+++ StudyHelp/paritycheck-matrix.py
@@ -0,0 +1,10 @@
+import sympy
+import numpy as np
+
+g = np.array([[1, 0, 1, 0, 1, 0],
+       [0, 1, 1, 0, 1, 0],
+       [0, 0, 0, 1, 1, 1]])
+
+h = np.array(sympy.Matrix(g).nullspace()) % 2
+
+print(h)
