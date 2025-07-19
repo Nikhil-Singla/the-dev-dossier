9bdbb3b4988ee04021664b7ac2eaf073d1883fb7
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-07-18 20:06:51 -0700

    Added cleaner comments and removed extra code.

--- testClicker/clickerTutorial_pygame.py
+++ testClicker/clickerTutorial_pygame.py
@@ -1,70 +1,65 @@
-## To use Python in CMD, use py instead of python
-## Idle clicker game
-## Following tutorial by : LeMaster Tech on YouTube
+## Idle Clicker Game (Based on LeMaster Tech's YouTube Tutorial)
+## Run in CMD using: py yourfilename.py
 
 import pygame  
 pygame.init()
 
+# Constants
 FIRST_ITEM = 0
 SECOND_ITEM = 1
 
-## Color Palette = (R, G, B) values
-aqua = (0,255,255)
-navy = (0,0,205)
-black = (0,0,0)
-skin = (255,228,196)
-orange = (255,97,3)
-metallic = (198,226,255)
+# Color Palette (RGB)
+aqua = (0, 255, 255)
+navy = (0, 0, 205)
+black = (0, 0, 0)
+skin = (255, 228, 196)
+orange = (255, 97, 3)
+metallic = (198, 226, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 white = (255, 255, 255)
-## End Palette
 
-screen = pygame.display.set_mode([1080, 720]) ## Resolution
-pygame.display.set_caption('Not Adventure Capitalist') ## Title of the game
+# Display Setup
+screen = pygame.display.set_mode([1080, 720])    # Resolution
+pygame.display.set_caption('Not Adventure Capitalist')  # Window Title
+background = black
+frameRate = 60
 
-background = black  ## Variable that can be edited later
-frameRate = 60  ## Framerate 
+# Fonts and Timing
+font = pygame.font.Font('freesansbold.ttf', 16)
+timer = pygame.time.Clock()
 
-font = pygame.font.Font('freesansbold.ttf', 16) ## Font
-timer = pygame.time.Clock() ## Help run our game at 60 FPS
-
+# Button Configuration
 buttonColorsOrder = [white, red, aqua, orange, navy]
 verticalPositionOrder = [100, 200, 300, 400, 500]
 horizontalPositionOrder = [10, 80, 150, 220, 290]
 
+# Upgrade Scaling Factors: (pre-lategame, post-lategame)
 buttonUpgradeScalingFactor = [(1.2, 1.1), (1.35, 1.25), (1.5, 1.4), (1.8, 1.6), (2, 1.6)]
-# Multiplicative scaling factor that decides the cost of the button. The first number is before the lategame threshold is reached, 
-# and the second number is after the lategame threshold is crossed.
 
+# Money Increase per Upgrade: (pre-lategame, post-lategame)
 moneyIncreasePerUpgrade = [(1, 2), (1.5, 3), (5, 10), (30, 40), (100, 200)]
-# Additive factor that increases the output of buttons after upgrade. The first number is for the beginner upgrade, and then the
-# second number takes effect once the late game threshold has been crossed in the cost of upgrades.For Example:
-# Button 1 increases output by 1 per upgrade, till it costs more than 500, then it increases output by 2 per upgrade
 
-lateGameThreshold = [500,1000,1500,2000,5000]
-# Late game threhold. As long as button cost is below this number, that button hasn't reached late game.
+# Lategame Thresholds per Button
+lateGameThreshold = [500, 1000, 1500, 2000, 5000]
 
+# Game State Variables
+moneyOnButtonPress = [1, 2, 3, 4, 5]
+barFillingSpeed = [5, 4, 3, 2, 1]
+percentFillForI = [0] * 5
+drawBarFilling = [False] * 5
+totalMoneyEarned = 0
 
-## Game Variables
+# Manager State
+managerOwned = [False] * 5
+managerCost = [100, 500, 1000, 3000, 5000]
+buttonUpgradeCost = list(range(1, 6))
 
-moneyOnButtonPress = [1, 2, 3, 4, 5]    # Upgrade of Box One, Two, Three. . .    Tells about the money gained on button press
-barFillingSpeed = [5, 4, 3, 2, 1]       # Upgrade of speed_One, Two, Three. . .  Tells about the speed of the bar filling
-percentFillForI = [0] * 5               # Upgrade of length_One , Two, Three. . .Tells of the current filling percent
-totalMoneyEarned = 0                    # Upgrade of Score                       Tells of the total money earned
-drawBarFilling = [False] * 5            # Upgrade of draw_One , Two, Three. . .  Tells if the bar is being drawn
-
-## End Game Variables
-
-## Manager Variables
-managerOwned = [False] * 5                  # Upgrade of manOneOwn , Two, Three. . . Tells if the manager is owned 
-managerCost = [100, 500, 1000, 3000, 5000]  # Upgrade of manOneCost, Two, Three. . . Tells what the cost of the manager is
-buttonUpgradeCost = list(range(1, 6))       # Upgrade of oneCost , Two ,  Three. . . Tells about the current and start cost of the button upgrade.
-
-## End Manager Variables
-
+# Drawing Function for the Main Clicker Box
 def draw_Box(color, y_cord, value, draw, length, speed):
     global totalMoneyEarned
+
+    # Update fill bar
     if draw and length < 200:
         length += speed
     elif length >= 200:
@@ -72,130 +67,111 @@ def draw_Box(color, y_cord, value, draw, length, speed):
         length = 0
         totalMoneyEarned += value
 
-    pygame.draw.rect(screen, color, [70, y_cord-15, 200, 30], 2)
-    pygame.draw.rect(screen, color, [70, y_cord-15, length, 30])
+    # Draw box outline and fill
+    pygame.draw.rect(screen, color, [70, y_cord - 15, 200, 30], 2)
+    pygame.draw.rect(screen, color, [70, y_cord - 15, length, 30])
+    
+    # Draw clickable circle
     task = pygame.draw.circle(screen, color, (30, y_cord), 20, 5)
+    
+    # Show money value on the button
+    value_text = font.render(str(round(value, 1)), True, white)
+    screen.blit(value_text, (16, y_cord - 10))
 
-    value_text = font.render(str(round(value,1)), True, white)
-    screen.blit(value_text, (16, y_cord-10))
     return task, length, draw
 
+# Drawing Function for Upgrade and Manager Buttons
 def newButton(color, x_coord, cost, manCost, owned):
+    # Upgrade button
     color_button = pygame.draw.rect(screen, color, [x_coord, 600, 50, 30])
-    color_cost = font.render(str(round(cost,1)), True, black)
-    screen.blit(color_cost, (x_coord+6, 605))
+    color_cost = font.render(str(round(cost, 1)), True, black)
+    screen.blit(color_cost, (x_coord + 6, 605))
+
+    # Manager button (disabled if already owned)
     if not owned:
         managerButton = pygame.draw.rect(screen, color, [x_coord, 670, 50, 30])
-        managerText = font.render(str(round(manCost,1)), True, black)
-        screen.blit(managerText, (x_coord+6, 675))
+        managerText = font.render(str(round(manCost, 1)), True, black)
+        screen.blit(managerText, (x_coord + 6, 675))
     else:
         managerButton = pygame.draw.rect(screen, black, [x_coord, 670, 50, 30])
+
     return color_button, managerButton
 
-gameState = True ## Game Running
+# ---------------------- Game Loop ----------------------
+gameState = True
 while gameState:
-    timer.tick(frameRate) ## Tick at the specified framerate
+    timer.tick(frameRate)
+
+    # Managers auto-click their assigned button
     for i in range(5):
         if managerOwned[i] and not drawBarFilling[i]:
             drawBarFilling[i] = True
-        
+
+    # Event Handling
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: ## Different from quit(). Here, its an event
+        if event.type == pygame.QUIT:
             gameState = False
-        
-        if event.type == pygame.MOUSEBUTTONDOWN:
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Handle manual clicks on buttons
             for i in range(5):
                 if tasks[i].collidepoint(event.pos):
                     drawBarFilling[i] = True
-                
 
+            # Handle manager purchases
             for i in range(5):
-                if managerBuyButtons[i].collidepoint(event.pos) and totalMoneyEarned >= managerCost[i] and not managerOwned[i]:
+                if (managerBuyButtons[i].collidepoint(event.pos)
+                    and totalMoneyEarned >= managerCost[i]
+                    and not managerOwned[i]):
                     managerOwned[i] = True
                     totalMoneyEarned -= managerCost[i]
 
-            
+            # Handle upgrade purchases
             for i in range(5):
-                if buyButtons[i].collidepoint(event.pos) and totalMoneyEarned >= buttonUpgradeCost[i]:
+                if (buyButtons[i].collidepoint(event.pos)
+                    and totalMoneyEarned >= buttonUpgradeCost[i]):
+
+                    totalMoneyEarned -= buttonUpgradeCost[i]
+
                     if buttonUpgradeCost[i] < lateGameThreshold[i]:
                         moneyOnButtonPress[i] += moneyIncreasePerUpgrade[i][FIRST_ITEM]
-                        totalMoneyEarned -= buttonUpgradeCost[i]
                         buttonUpgradeCost[i] *= buttonUpgradeScalingFactor[i][FIRST_ITEM]
                     else:
                         moneyOnButtonPress[i] += moneyIncreasePerUpgrade[i][SECOND_ITEM]
-                        totalMoneyEarned -= buttonUpgradeCost[i]
                         buttonUpgradeCost[i] *= buttonUpgradeScalingFactor[i][SECOND_ITEM]
 
-            # if buy2.collidepoint(event.pos) and totalMoneyEarned >= buttonUpgradeCost[1]:
-            #     if buttonUpgradeCost[1] < 1000:
-            #         moneyOnButtonPress[1] += 1.5
-            #         totalMoneyEarned -= buttonUpgradeCost[1]
-            #         buttonUpgradeCost[1] *= 1.35
-            #     else:
-            #         moneyOnButtonPress[1] += 3
-            #         totalMoneyEarned -= buttonUpgradeCost[1]
-            #         buttonUpgradeCost[1] *= 1.25
+    # Drawing Phase
+    screen.fill(background)
 
-            # if buy3.collidepoint(event.pos) and totalMoneyEarned >= buttonUpgradeCost[2]:
-            #     if buttonUpgradeCost[2] < 1500:
-            #         moneyOnButtonPress[2] += 5
-            #         totalMoneyEarned -= buttonUpgradeCost[2]
-            #         buttonUpgradeCost[2] *= 1.5
-            #     else:
-            #         moneyOnButtonPress[2] += 10
-            #         totalMoneyEarned -= buttonUpgradeCost[2]
-            #         buttonUpgradeCost[2] *= 1.4
-
-            # if buy4.collidepoint(event.pos) and totalMoneyEarned >= buttonUpgradeCost[3]:
-            #     if buttonUpgradeCost[3] < 2000:
-            #         moneyOnButtonPress[3] += 30
-            #         totalMoneyEarned -= buttonUpgradeCost[3]
-            #         buttonUpgradeCost[3] *= 1.8
-            #     else:
-            #         moneyOnButtonPress[3] += 40
-            #         totalMoneyEarned -= buttonUpgradeCost[3]
-            #         buttonUpgradeCost[3] *= 1.6
-
-            # if buy5.collidepoint(event.pos) and totalMoneyEarned >= buttonUpgradeCost[4]:
-            #     if buttonUpgradeCost[4] < 5000:
-            #         moneyOnButtonPress[4] += 100
-            #         totalMoneyEarned -= buttonUpgradeCost[4]
-            #         buttonUpgradeCost[4] *= 2
-            #     else:
-            #         moneyOnButtonPress[4] += 200
-            #         totalMoneyEarned -= buttonUpgradeCost[4]
-            #         buttonUpgradeCost[4] *= 1.6
-            
-            
-    screen.fill(background) ## Have our initial background on the screen
-    
+    # Draw clicker bars
     tasks = []
     for i in range(5):
-        tempTask, percentFillForI[i], drawBarFilling[i] = draw_Box(buttonColorsOrder[i], verticalPositionOrder[i], moneyOnButtonPress[i], drawBarFilling[i], percentFillForI[i], barFillingSpeed[i])
-        tasks.append(tempTask)
+        task, percentFillForI[i], drawBarFilling[i] = draw_Box(
+            buttonColorsOrder[i], verticalPositionOrder[i],
+            moneyOnButtonPress[i], drawBarFilling[i],
+            percentFillForI[i], barFillingSpeed[i]
+        )
+        tasks.append(task)
 
-    buyButtons, managerBuyButtons = [], []
-    
+    # Draw upgrade and manager buttons
+    buyButtons = []
+    managerBuyButtons = []
     for i in range(5):
-        tempBuyButton, tempManagerBuyButton = newButton(buttonColorsOrder[i], horizontalPositionOrder[i], buttonUpgradeCost[i], managerCost[i], managerOwned[i])
-        buyButtons.append(tempBuyButton)
-        managerBuyButtons.append(tempManagerBuyButton)
+        buyBtn, manBtn = newButton(
+            buttonColorsOrder[i], horizontalPositionOrder[i],
+            buttonUpgradeCost[i], managerCost[i], managerOwned[i]
+        )
+        buyButtons.append(buyBtn)
+        managerBuyButtons.append(manBtn)
 
-    # buy1, _ = newButton(white, 10, buttonUpgradeCost[0], managerCost[0], managerOwned[0])
-    # buy2, _ = newButton(red, 80, buttonUpgradeCost[1], managerCost[1], managerOwned[1])
-    # buy3, _ = newButton(aqua, 150, buttonUpgradeCost[2], managerCost[2], managerOwned[2])
-    # buy4, _ = newButton(orange, 220, buttonUpgradeCost[3], managerCost[3], managerOwned[3])
-    # buy5, _ = newButton(navy, 290, buttonUpgradeCost[4], managerCost[4], managerOwned[4])
+    # Display HUD
+    displayMoney = font.render(f'Money: ${round(totalMoneyEarned, 2)}', True, white, black)
+    screen.blit(displayMoney, (10, 5))
 
-    displayMoney = font.render('Money: $'+str(round(totalMoneyEarned,2)), True, white, black)
-    screen.blit(displayMoney, (10,5))
+    screen.blit(font.render('Buy More: ', True, white), (10, 580))
+    screen.blit(font.render('Buy Managers: ', True, white), (10, 650))
 
-    buyMore = font.render('Buy More: ', True, white)
-    screen.blit(buyMore, (10, 580))
+    pygame.display.flip()  # Refresh screen
 
-    buyMoreMan = font.render('Buy Managers: ', True, white)
-    screen.blit(buyMoreMan, (10, 650))
-
-    pygame.display.flip() ## Update the content of the entire display
-
-pygame.quit() ## Uninitialize all pygame modules
+# Exit Game
+pygame.quit()
