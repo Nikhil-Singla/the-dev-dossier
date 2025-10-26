2e9ab6251fa1b0d7ccb3c4592fdaa4e3cc75923c
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-10-26 00:00:49 -0700

    [002-CopyHistory Tool] Testing copy-paste

--- 01_WorkInProgress/CopyHistory/CopyHistory.py
+++ 01_WorkInProgress/CopyHistory/CopyHistory.py
@@ -1,32 +1,50 @@
 from pynput.keyboard import Key, Listener
 import win32clipboard
 
-def setClipboard(text):
-    win32clipboard.OpenClipboard()
+COPY = '\x03'
+CUT = '\x18'
+PASTE = '\x16'
 
+def setClipboard(text):
+    '''Opens the clipboard, empties it, and sets the text to the parameter "text".
+    
+    :param text: Input text that will be replaced to clipboard
+    :return: None. The clipboard should be replaced.
+    '''
+    win32clipboard.OpenClipboard()
     win32clipboard.EmptyClipboard()
+
     win32clipboard.SetClipboardText(str(text))
 
     win32clipboard.CloseClipboard()
 
 def getFromClipboard():
-    win32clipboard.OpenClipboard()
-    data = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
+    try:
+        win32clipboard.OpenClipboard()
+        data = win32clipboard.GetClipboardData()
+    except Exception as e:
+        print(f"Error accessing clipboard: {e}")
+
+    finally:
+        win32clipboard.CloseClipboard()
+    
     return data
 
 def on_press(key):
     print('{0} pressed'.format(key))
 
-
 def on_release(key):
     print('{0} release'.format(key))
-    if key == Key.esc:
-        return False
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+    if key == COPY:
+        print(getFromClipboard())
 
-print(getFromClipboard())
-setClipboard("Whatever")
-print(getFromClipboard())
+listener = Listener(on_press=on_press, on_release=on_release)
+
+listener.start()
+input()
+listener.stop()
+
+# print(getFromClipboard())
+# setClipboard("Whatever")
+# print(getFromClipboard())
